dedc12b5ffa14ee9caa659d9f9427a84b68cdc09
Cody Lawson <CLawson@esri.com>
2016-05-24 13:13:54 -0700

    Add highlighting for the current day in the pay period

--- script.js
+++ script.js
@@ -14,38 +14,62 @@ $.each(
 
         //need to skip all this if the wbs is something like vacation or holiday
         if (ignoredIds.indexOf(wbs) < 0) {
-          //create the links because we know we have at least some details
-          var hasProjectDetails = wbs[0] === 'C';
-          var projectWbs;
+            //create the links because we know we have at least some details
+            var hasProjectDetails = wbs[0] === 'C';
+            var projectWbs;
 
-          //format the project id for other pages to search
-          //trims the id and adds '.'
-          if (hasProjectDetails) {
-            projectWbs = wbs.substring(0, wbs.indexOf('A'));
-            projectWbs = [projectWbs.slice(0, 1), '.', projectWbs.slice(1)].join('');
-          }
+            //format the project id for other pages to search
+            //trims the id and adds '.'
+            if (hasProjectDetails) {
+                projectWbs = wbs.substring(0, wbs.indexOf('A'));
+                projectWbs = [projectWbs.slice(0, 1), '.', projectWbs.slice(1)].join('');
+            }
 
-          //create domNodes for details and related wbs
-          var relatedWbsLink;
-          if (hasProjectDetails) {
-            relatedWbsLink = baseRelatedLink + relatedWbsLink + projectWbs;
-          } else {
-            relatedWbsLink = baseRelatedLink + relatedIoLink + wbs;
-          }
-          $(value).append('<a href="' + relatedWbsLink + '" target="_blank" class="wbs-link related-wbs-link" data-tooltip-text="' + relatedTooltip + '"><i class="fa fa-list"></i></a>');
+            //create domNodes for details and related wbs
+            var relatedWbsLink;
+            if (hasProjectDetails) {
+                relatedWbsLink = baseRelatedLink + relatedWbsLink + projectWbs;
+            } else {
+                relatedWbsLink = baseRelatedLink + relatedIoLink + wbs;
+            }
+            $(value).append('<a href="' + relatedWbsLink + '" target="_blank" class="wbs-link related-wbs-link" data-tooltip-text="' + relatedTooltip + '"><i class="fa fa-list"></i></a>');
 
-          //now for the details page link
-          if (hasProjectDetails) {
-            var projectDetailsLink = baseDetailsLink + projectWbs;
-            $(value).append('<a href="' + projectDetailsLink + '" target="_blank" class="wbs-link details-wbs-link" data-tooltip-text="' + detailsTooltip + '"><i class="fa fa-info-circle"></i></a>');
-          }
+            //now for the details page link
+            if (hasProjectDetails) {
+                var projectDetailsLink = baseDetailsLink + projectWbs;
+                $(value).append('<a href="' + projectDetailsLink + '" target="_blank" class="wbs-link details-wbs-link" data-tooltip-text="' + detailsTooltip + '"><i class="fa fa-info-circle"></i></a>');
+            }
         }
+    }
+);
+
+var contentRows = 'form > table > tbody > tr > td > table:nth-child(0n + 4) > tbody > tr > td:last-child > table:nth-child(0n + 2) > tbody > tr';
+var todaysColIndex;
+//loop through the header rows and get the index for the current date
+$.each(
+    $(contentRows + ':nth-child(0n + 2) td'),
+    function(index, value) {
+      var formattedDay = ("0" + new Date().getDate()).slice(-2);
+      var formmatedMonth = ("0" + (new Date().getMonth() + 1)).slice(-2);
+      var formattedDate = formmatedMonth + '/' + formattedDay;
+
+      if ($(value).find('b:first-child').html() == formattedDate) {
+          //this is the current day column, use this index for adding classes to the other rows
+          todaysColIndex = index + 1;
+      }
+    }
+);
+//add a class to the column for todays date
+$.each(
+    $(contentRows + '>td:nth-child(0n+' + todaysColIndex + ')'),
+    function(index, value) {
+      $(value).addClass('today-col');
     }
 );
 
 var hrefStrings = ['history', 'javascript:Vacation', 'javascript:Sick', 'javascript:bank', 'javascript:Jury', 'audit', 'javascript:Password', 'ts_summary', 'ts_query', 'ext_hrs_details'];
 var iconStrings = ['history', 'glass', 'frown-o', 'university', 'gavel', 'eye', 'lock', 'list-alt', 'search', 'clock-o'];
 $.each(hrefStrings, function(index, value) {
-  var el = $('a[href^="' + value + '"] b');
-  $(el).attr('class', 'menu-link link-icon-' + iconStrings[index]);
+    var el = $('a[href^="' + value + '"] b');
+    $(el).attr('class', 'menu-link link-icon-' + iconStrings[index]);
 });
