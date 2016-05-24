b066ce284511138b3690518acbc754f6797b3cf1
Cody Lawson <CLawson@esri.com>
2016-05-23 19:47:44 -0700

    Add separate links for related was numbers and project details, some more styling changes

--- script.js
+++ script.js
@@ -1,30 +1,45 @@
-// Here You can type your custom JavaScript...
 $.each(
     $('td[id^="chgRow"] div'),
     function(index, value) {
-        $(value).click(function() {
-            var url;
-            var wbs = $(value).attr('id');
-            if (wbs[0] === 'C') {
-                //is a project wbs
-                var projectUrl = 'http://pswebsp:85/pdb/ProjectOverview.asp?projnum=';
-                wbs = wbs.substring(0, wbs.indexOf('A'));
-                var projectWbs = [wbs.slice(0, 1), '.', wbs.slice(1)].join('');
-                url = projectUrl + projectWbs;
-            } else {
-                //some other kind of wbs
-                url = 'http://arczone.esri.com/utilities/finder/index.cfm?fa=io&wbs=' + $(value).attr('id');
-            }
+        //link strings
+        var baseRelatedLink = 'http://arczone.esri.com/utilities/finder/index.cfm?';
+        var relatedWbsLink = 'fa=wbs&wbs=';
+        var relatedIoLink = 'fa=io&io=';
+        var baseDetailsLink = 'http://pswebsp:85/pdb/ProjectOverview.asp?projnum=';
+        var relatedTooltip = 'View Related Numbers';
+        var detailsTooltip = 'View Project Details';
+        var ignoredIds = ['BERE', 'COMP', 'JURY', 'SICK', 'VACA', 'NOPA', 'HOLI'];
 
-            var win = window.open(url, '_blank');
-            if(win){
-                //Browser has allowed it to be opened
-                win.focus();
-            }else{
-                //Broswer has blocked it
-                alert('Please allow popups to see details about this WBS');
-            }
-        })
+        var wbs = $(value).attr('id');
+
+        //need to skip all this if the wbs is something like vacation or holiday
+        if (ignoredIds.indexOf(wbs) < 0) {
+          //create the links because we know we have at least some details
+          var hasProjectDetails = wbs[0] === 'C';
+          var projectWbs;
+
+          //format the project id for other pages to search
+          //trims the id and adds '.'
+          if (hasProjectDetails) {
+            projectWbs = wbs.substring(0, wbs.indexOf('A'));
+            projectWbs = [projectWbs.slice(0, 1), '.', projectWbs.slice(1)].join('');
+          }
+
+          //create domNodes for details and related wbs
+          var relatedWbsLink;
+          if (hasProjectDetails) {
+            relatedWbsLink = baseRelatedLink + relatedWbsLink + projectWbs;
+          } else {
+            relatedWbsLink = baseRelatedLink + relatedIoLink + wbs;
+          }
+          $(value).append('<a href="' + relatedWbsLink + '" target="_blank" class="wbs-link related-wbs-link" data-tooltip-text="' + relatedTooltip + '"><i class="fa fa-list"></i></a>');
+
+          //now for the details page link
+          if (hasProjectDetails) {
+            var projectDetailsLink = baseDetailsLink + projectWbs;
+            $(value).append('<a href="' + projectDetailsLink + '" target="_blank" class="wbs-link details-wbs-link" data-tooltip-text="' + detailsTooltip + '"><i class="fa fa-info-circle"></i></a>');
+          }
+        }
     }
 );
 
